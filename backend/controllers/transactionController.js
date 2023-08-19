466bdbc0c233d251e0812214341a112d05e202e1
Albert Reis <albertlreis@gmail.com>
2023-08-19 05:27:15 -0300

    Adicionar suporte a paginação na rota de listagem de transações

--- backend/controllers/transactionController.js
+++ backend/controllers/transactionController.js
@@ -2,8 +2,22 @@ import Transaction from '../models/Transaction.js';
 
 export const getAllTransactions = async (req, res) => {
     try {
-        const transactions = await Transaction.findAll();
-        res.status(200).json(transactions);
+        const page = parseInt(req.query.page) || 1;
+        const pageSize = parseInt(req.query.pageSize) || 10;
+
+        const totalCount = await Transaction.count();
+        const totalPages = Math.ceil(totalCount / pageSize);
+
+        const transactions = await Transaction.findAll({
+            offset: (page - 1) * pageSize,
+            limit: pageSize,
+        });
+
+        res.json({
+            data: transactions,
+            totalPages,
+            currentPage: page,
+        });
     } catch (error) {
         console.error('Erro ao buscar transações:', error);
         res.status(500).json({ error: 'Erro ao buscar transações.' });
